8273dc308998e8d737f6133f244e5299e051149d
Ashkan Nasseri <ashkan@artsymail.com>
2019-08-05 10:14:44 -0400

    add buttons

--- assets/js/pages/placeDetail.tsx
+++ assets/js/pages/placeDetail.tsx
@@ -1,11 +1,14 @@
 import * as React from "react"
-import { Spinner, Flex, Box, Button, Serif, BorderBox, space, color, Col, Image } from "@artsy/palette"
+import { Spinner, Flex, Box, Button, Serif, BorderBox, space, color, Col, Image, Sans } from "@artsy/palette"
 
 import Header from "../components/header";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
-
+interface State {
+  dobar: boolean
+  rideShareDobar: boolean
+}
 interface Props {
   match: any
 }
@@ -20,7 +23,11 @@ const FIND_PLACE_QUERY = gql`
   }
 `
 
-export default class PlaceDetail extends React.Component<Props, {}>{
+export default class PlaceDetail extends React.Component<Props, State>{
+  public constructor(props: Props, context: any) {
+    super(props, context)
+    this.state = { dobar: false, rideShareDobar: false }
+  }
   public render(){
     return(
       <Query query={FIND_PLACE_QUERY} variables={{id: this.props.match.params.placeId}}>
@@ -31,10 +38,16 @@ export default class PlaceDetail extends React.Component<Props, {}>{
               { loading && <Spinner size="large"/>}
               { error && <> Error! {error} </>}
               { !error && !loading && data &&
-                <Flex flexDirection="row" justifyContent="space-between">
-                  <BorderBox flexDirection="column" style={{width: 200}}>
-                    {data.place.name}
-                  </BorderBox>
+                <Flex flexDirection="column" justifyContent="space-between">
+                  <Sans size={4}>{data.place.name}</Sans>
+                  <Sans size={2}>Would you go to this place again?</Sans>
+                  <Button variant="secondaryOutline">Yes</Button>
+                  {this.state.dobar &&
+                  <>
+                    <Sans size={2}>You are 3 miles away from {data.place.name}, would you use a rideshare service to go there?</Sans>
+                    <Button disabled={this.state.dobar && !this.state.rideShareDobar} variant="secondaryOutline">Yes</Button>
+                  </>
+                  }
                 </Flex>
               }
             </>
