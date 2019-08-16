cfaefd18a2eb9cfa3dbc02c68f100adb9f417ae7
ashkan18 <ashkan18@gmail.com>
2019-08-15 19:30:53 -0700

    Add user info

--- assets/js/components/questions.tsx
+++ assets/js/components/questions.tsx
@@ -2,12 +2,20 @@ import * as React from "react"
 import { Spinner, Flex, Sans, CheckIcon, CloseIcon } from "@artsy/palette"
 
 import gql from "graphql-tag";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 import { useState } from "react";
 
 interface Props {
   place: any
 }
+
+const ME_QUERY = gql`
+  query {
+    me {
+      name
+    }
+  }
+`
 
 const DOBAR_MUTATION  = gql`
   mutation Dobar($placeId: ID!, $response: Boolean) {
@@ -26,6 +34,7 @@ const RIDESHARE_DOBAR_MUTATION  = gql`
 
 export const Questions = (props: Props) => {
   const {place} = props
+  const {loading: meLoading, data: {me}} = useQuery(ME_QUERY)
   const [haveBeenToPlace, setHaveBeenToPlace] = useState(false)
   const [dobar, setDobar] = useState(false)
   const [rideshare, setRideshare] = useState(false)
@@ -42,7 +51,7 @@ export const Questions = (props: Props) => {
   return(
     <Flex flexDirection="column" justifyContent="space-between">
       <Flex flexDirection="row">
-        <Sans size={5}>Have you been to {place.name}?</Sans>
+        <Sans size={5}>{me && `${me.name}, `} Have you been to {place.name}?</Sans>
         <CheckIcon ml={2} height={30} width={30} opacity={haveBeenToPlace === true ? 1 : 0.2} onClick={ _e => setHaveBeenToPlace(true)}/>
       </Flex>
       {haveBeenToPlace &&
