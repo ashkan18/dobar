32c1757fa3caeeb06bff1ed3e7f01114d2e6e009
Ashkan Nasseri <ashkan.nasseri@gmail.com>
2019-11-27 13:40:38 -0500

    increase footer

--- assets/js/components/footer.tsx
+++ assets/js/components/footer.tsx
@@ -1,17 +1,17 @@
-import { Flex, Spacer, Separator, Link } from '@artsy/palette'
+import { Flex, Spacer, Separator, Link, Box } from '@artsy/palette'
 import React from "react"
 
 
 const Footer = () => {
   return (
-    <>
+    <Box mt={6}>
       <Separator mb={2} mt={1}/>
       <Flex FlexDirection="row" flexWrap="nowrap" justifyContent="center" alignContent="space-around">
         <Link href="about" m={1}>About</Link>
         <Link href="terms" m={1}>Terms</Link>
       </Flex>
       <div style={{textAlign: 'center'}}>© 2019 Dobar All Rights Reserved. </div>
-    </>
+    </Box>
   )
 }
 
